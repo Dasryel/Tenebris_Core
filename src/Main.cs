f2f01922aab2853f398b1d269a896ad4f4fec716
Antti Hakkarainen <a.hakkarainen@iki.fi>
2026-02-22 21:49:08 +0200

    feat: mapmanager loadmap function

--- src/Main.cs
+++ src/Main.cs
@@ -18,7 +18,23 @@ namespace GameProject
             AddChild(this._mapManager);
 
             GD.Print("Main initialized");
+
+            _mapManager.LoadMap("scene/map/world/debug.tscn");
         }
 
+
+        /*
+        Manual cleanup due to singletons etc
+        */
+        public override void _ExitTree()
+        {
+            this._mapManager?.Free();
+            this._mapManager = null;
+
+            this._signalBus?.Free();
+            this._signalBus = null;
+
+            PrintOrphanNodes();
+        }
     }
 }
--- src/core/MapManager.cs
+++ src/core/MapManager.cs
@@ -8,10 +8,30 @@ namespace GameProject
         private static readonly MapManager _instance = new();
         public static MapManager Instance => _instance;
 
+        private static Node _currentMapInstance;
+
         public override void _Ready()
         {
             this.Name = "MapManager";
+            GD.Print("[MapManager] instance created");
         }
 
+
+        public bool LoadMap(string scenePath)
+        {
+            if (!FileAccess.FileExists(scenePath))
+            {
+                GD.PrintErr($"[MapManager] Map path not found: {scenePath}");
+                return false;
+            }
+
+            PackedScene mapScene = ResourceLoader.Load<PackedScene>(scenePath);
+            _currentMapInstance = mapScene.Instantiate();
+
+            AddChild(_currentMapInstance);
+            GD.Print($"[MapManager] Loaded map: {scenePath}");
+
+            return true;
+        }
     }
 }
