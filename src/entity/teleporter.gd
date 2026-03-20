7f8822f9b9328756cbf6a9cb4f402fbde35f96dc
Antti Hakkarainen <a.hakkarainen@iki.fi>
2026-03-20 02:23:32 +0200

    simplify teleporter logic and setup

--- src/entity/teleporter.gd
+++ src/entity/teleporter.gd
@@ -1,17 +1,20 @@
 extends Area2D
 
-## The scene this door leads to
 @export_file("*.tscn") var target_scene: String
-@export var entry_point_id: String = "Default"
 
+var _is_active: bool = true
 var _is_teleporting: bool = false
 
+func _ready() -> void:
+    if GameState.target_entry_point == self.name:
+        _is_active = false
+        get_tree().create_timer(0.1).timeout.connect(func(): _is_active = true)
+
 func _on_body_entered(body: Node2D) -> void:
-	if _is_teleporting or body is not Player:
-		return
+    if _is_teleporting or not _is_active or not body is Player:
+        return
 
-	_is_teleporting = true
-
-	GameState.target_entry_point = entry_point_id
-	set_deferred("monitoring", false)
-	get_tree().call_deferred("change_scene_to_file", target_scene)
+    _is_teleporting = true
+    GameState.target_entry_point = self.name
+    set_deferred("monitoring", false)
+    get_tree().call_deferred("change_scene_to_file", target_scene)
