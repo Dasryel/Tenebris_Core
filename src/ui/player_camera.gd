fe256f57dc3a4876d83e57f2fc2efbcec75801e5
Antti Hakkarainen <a.hakkarainen@iki.fi>
2026-03-29 11:28:37 +0300

    debug camera zoom for mouse wheel

--- src/ui/player_camera.gd
+++ src/ui/player_camera.gd
@@ -3,6 +3,12 @@ extends Camera2D
 
 var player: Player
 
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("mwheel_up"):
+        zoom += Vector2(0.1, 0.1)
+    elif event.is_action_pressed("mwheel_down"):
+        zoom -= Vector2(0.1, 0.1)
+
 func _init(current_player: Player):
     player = current_player
 
