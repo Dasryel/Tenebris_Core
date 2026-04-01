79ceddd2d0c4dc7e72d256d2c7035f1b66d34396
Antti Hakkarainen <a.hakkarainen@iki.fi>
2026-04-01 13:40:19 +0300

    anims for lava enemy

--- src/entity/lava_enemy.gd
+++ src/entity/lava_enemy.gd
@@ -1,6 +1,7 @@
 class_name LavaEnemy
 extends Enemy
 
+@export_group("Jump parameters")
 @export var jump_height: float = 200.0
 @export var jump_duration: float = 0.5
 @export var jump_interval: float = 3.0
@@ -10,40 +11,52 @@ var _original_y: float = 0.0
 
 
 func _ready() -> void:
-    _original_y = global_position.y
-    _jump_loop()
+	_original_y = global_position.y
+	sprite.play("idle")
+	_jump_loop()
 
 
 func _jump_loop() -> void:
-    while true:
-        await get_tree().create_timer(jump_interval).timeout
-        await _jump()
+	while true:
+		await get_tree().create_timer(jump_interval).timeout
+		await _jump()
 
 
 func _jump() -> void:
-    if _is_jumping:
-        return
+	if _is_jumping:
+		return
 
-    _is_jumping = true
+	_is_jumping = true
+	sprite.play("jump")
 
-    # Go up
-    var tween := create_tween()
-    tween.tween_property(self , "global_position:y", _original_y - jump_height, jump_duration) \
-        .set_ease(Tween.EASE_OUT) \
-        .set_trans(Tween.TRANS_SINE)
-    await tween.finished
+	# Go up
+	var tween := create_tween()
+	tween.tween_property(self , "global_position:y", _original_y - jump_height, jump_duration) \
+		.set_ease(Tween.EASE_OUT) \
+		.set_trans(Tween.TRANS_SINE)
+	await tween.finished
 
-    # Come down
-    tween = create_tween()
-    tween.tween_property(self , "global_position:y", _original_y, jump_duration) \
-        .set_ease(Tween.EASE_IN) \
-        .set_trans(Tween.TRANS_SINE)
-    await tween.finished
+	# Come down
+	tween = create_tween()
+	tween.tween_property(self , "global_position:y", _original_y, jump_duration) \
+		.set_ease(Tween.EASE_IN) \
+		.set_trans(Tween.TRANS_SINE)
+	await tween.finished
 
-    _is_jumping = false
+	sprite.play("idle")
+	_is_jumping = false
+
+
+func _on_entity_death() -> void:
+	sprite.play("die")
+	set_process(false)
+	set_physics_process(false)
+	# Wait for the death animation to finish before removing
+	await sprite.animation_finished
+	queue_free()
 
 
 func _on_player_damage_area_2d_body_entered(body: Node2D) -> void:
-    if body.is_in_group("player"):
-        body.take_damage(1, Vector2(0, 0))
-        await self.get_tree().create_timer(0.75).timeout
+	if body.is_in_group("player"):
+		body.take_damage(1, Vector2(0, 0))
+		await get_tree().create_timer(0.75).timeout
