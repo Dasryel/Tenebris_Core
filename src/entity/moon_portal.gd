bedec6e28c0b10cb9249d1914893dc8794900e21
Antti Hakkarainen <a.hakkarainen@iki.fi>
2026-04-11 18:21:06 +0300

    add pixelated reveal animation when adding moon pieces

--- src/entity/moon_portal.gd
+++ src/entity/moon_portal.gd
@@ -1,19 +1,25 @@
 extends Area2D
 
 @export var sprites: Array[Texture2D]
+@export var moon_sprite: Sprite2D
+@export var reveal_duration: float = 2.5
 
-var near_player: bool = false
+var shader_mat := ShaderMaterial.new()
+var _near_player: bool = false
 
 func _ready() -> void:
-	update_sprite()
-	$Sprite2D.texture = sprites[GameState.moon_phase]
+	moon_sprite.texture = sprites[GameState.moon_phase]
+	shader_mat.shader = load("res://resource/shader/add_moon_phase.gdshader")
+	moon_sprite.material = shader_mat
 
-func _process(_delta: float) -> void:
-	if near_player and Input.is_action_just_pressed("use"):
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("use") and _near_player:
 		if GameState.moon_phase >= 3:
 			activate_portal()
 		elif GameState.moon_pieces_count() > 0:
 			consume_piece()
+
 
 func consume_piece() -> void:
 	for id in GameState.moon_pieces:
@@ -27,9 +33,26 @@ func consume_piece() -> void:
 	SignalBus.moon_phase_changed.emit(GameState.moon_phase)
 	update_thought_bubble()
 
+
 func update_sprite() -> void:
-	if sprites.size() > GameState.moon_phase:
-		$Sprite2D.texture = sprites[GameState.moon_phase]
+	if sprites.size() <= GameState.moon_phase:
+		return
+
+	shader_mat.set_shader_parameter("previous_tex", moon_sprite.texture)
+	shader_mat.set_shader_parameter("noise_seed", randf_range(0.0, 999.0))
+
+	moon_sprite.texture = sprites[GameState.moon_phase]
+
+	if GameState.moon_phase == 0:
+		shader_mat.set_shader_parameter("reveal", 1.0)
+		return
+
+	var tween = create_tween()
+	tween.tween_method(
+		func(v): shader_mat.set_shader_parameter("reveal", v),
+		0.0, 1.0, reveal_duration
+	)
+
 
 func activate_portal() -> void:
 	print("portal activated!")
@@ -47,6 +70,7 @@ func activate_portal() -> void:
 	flash.tween_property($PointLight2D, "energy", 6.0, 0.5)
 	await flash.finished
 
+
 func update_thought_bubble() -> void:
 	if GameState.moon_phase >= 3:
 		SignalBus.thought_bubble_show.emit("Press E to teleport")
@@ -55,12 +79,14 @@ func update_thought_bubble() -> void:
 	else:
 		SignalBus.thought_bubble_show.emit("Seems like a portal... I need to find the pieces")
 
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		near_player = true
+	if body.is_in_group("player"):
+		_near_player = true
 		update_thought_bubble()
 
+
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "Player":
-		near_player = false
+	if body.is_in_group("player"):
+		_near_player = false
 		SignalBus.thought_bubble_hide.emit()
